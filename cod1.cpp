9f199bdb053caaa2af489f3bb19fb693069bc814
SteppanovDavid <vkpochta2@vk.com>
2024-09-25 13:02:01 +0300

    Add files via upload

---
+++ cod1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream> 
+#include<string>
+using namespace std;
+int main()
+/*
+* #include <iostream> #include<string>
+#include<windows.h> using namespace std;
+int main()
+{ 	if (c1  c3) { cout << "true" << endl;; }
+
+	if (a1 == 3); { cout <<  " << endl; cin >> b1; cout << " " << endl;
+		cin >> c1; 	cout << " " << s1 << endl; }
+	if (q1 ! 5) { cout << "" << endl; cin >> w1;
+		cout << "" << endl; cin >> e1;
+		r1 = ; cout << "" << r1 << endl; }
+	if (a1 > b1  c2 < c1) { cout << "true" << endl; }
+	else { cout << "false" << endl; } if (c5  c3 || c5  c3)
+	{ cout << "true" << endl; } else { cout << "false" << endl; }
+	int a1 = 60; int a2 = 60; int b1 = 25;  int c1 = a1 && b1;
+	int c2 = a1 || b1; int c3 = a1 -- b1; int c4 = a1 ++ b1; int c5 = a1 != b1;
+	int i = 0, b = 5;
+	while (i < 10 ++b < 10)
+	{ 	i++; i1 = i1 + i1;  i2 = i2 + i2;
+		cout << i1 << endl; cout << i2 << endl;   b++ ; } }
+}
+
+*/
+{
+	int a1 = 3;
+	int a2 = 60;
+	int b1 = 8;
+	int c1 = a1 + b1;
+	int c2 = a1 | b1;
+	int c3 = a1 - b1;
+	int c4 = a1 + b1;
+	int c5 = a1 / b1;
+	int i = 0, b = 5,i1=1,i2=2;
+		int w1, e1, r1,  s1;
+	
+		if (c1 > c3) { cout << "true" << endl;; }
+
+		if (a1 == 3)
+		{
+			cout << "b1=" << endl;
+			cin >> b1;
+			cout << "c1= " << endl;
+			cin >> s1;
+			cout << " s1=" << s1 << endl;
+		}
+		if (b1 != 5)
+		{
+			cout << "w1=" << endl; cin >> w1;
+			cout << "w2=" << endl; cin >> e1;
+			r1 = 3; cout << "r1=" << r1 << endl;
+		}
+		if (a1 < b1 && c2 < c1)
+		{
+			cout << "true" << endl;
+		}
+		else
+		{
+			cout << "false" << endl;
+		}
+		if (c5 < c3 || c5 > c3)
+		{
+			cout << "true" << endl;
+		}
+		else { cout << "false" << endl; }
+
+		while (i < 10 && b1 < 10)
+		{
+			i++;
+			i1 = i1 + i1;
+			i2 = i2 + i2;
+			cout << i1 << endl;
+			cout << i2 << endl;   
+			b++;
+		}
+	}
